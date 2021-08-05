0899affbafe778002da6eb30070cf72569d71163
jhoanrl93 <jaratival@unal.edu.co>
2021-08-04 19:53:02 -0500

    by country component complete

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
   styleUrls: ['./by-country.component.css']
 })
-export class ByCountryComponent implements OnInit {
+export class ByCountryComponent  {
 
-  constructor() { }
+  term: string ="Colombia";
+  error: boolean = false;
+  countries: Country[] = [];
 
-  ngOnInit(): void {
+  
+
+  constructor( private countryService: CountryService) { }
+
+  search(){
+    this.error = false;
+    console.log(this.term);
+    this.countryService.searchCountry (this.term).subscribe( (countries) =>{
+      this.countries = countries;
+    }, (err) => {
+      this.error = true;
+      this.countries = [];
+    }
+    );
+
   }
+
+  
 
 }
---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Country } from '../interfaces/country.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  private apiUrl: string = 'https://restcountries.eu/rest/v2';
+
+  constructor( private http: HttpClient) { }
+
+  searchCountry ( term: string): Observable<Country[]> {
+    
+    const url = `${ this.apiUrl }/name/${ term }`;
+    
+    return this.http.get<Country[]>(url);
+  }
+
+}
